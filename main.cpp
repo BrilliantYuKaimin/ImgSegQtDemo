03f86fe5b6632a29b89c5cc686b342d742b7cf04
BrilliantYuKaimin <91609464+BrilliantYuKaimin@users.noreply.github.com>
2022-11-23 14:07:01 +0800

    编译流程走通

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#include "paddle/include/paddle_inference_api.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    paddle_infer::Config config;
+    config.SetModel("../model.pdmodel", "../model.pdiparams");
+    auto predictor = paddle_infer::CreatePredictor(config);
     return 0;
 }
